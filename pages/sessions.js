b17e79dae7792a72cb649ac70758145076e1682d
Bramus <bramus@bram.us>
2021-09-02 15:32:23 +0200

    Sessions Overview

---
+++ pages/sessions.js
@@ -0,0 +1,45 @@
+import styles from '../styles/Home.module.css'
+
+const Sessions = ({ sessions }) => {
+    if (!sessions || !sessions.length) {
+        return (
+            <p>No Sessions</p>
+        );
+    }
+
+    return (
+        <div className={styles.container}>
+            <p>Got {sessions.length} Sessions:</p>
+            <ul>
+                {sessions.map(session => (
+                    <li key={session.slug}>“{session.title}” by {session.speaker.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    const res = await fetch(`https://ffconf.org/api/event/2018`);
+    const sessions = await res.json();
+
+    if (!sessions) {
+        return {
+            notFound: true,
+        }
+        // return {
+        //     redirect: {
+        //         destination: '/',
+        //         permanent: false,
+        //     },
+        // };
+    }
+
+    return {
+        props: {
+            sessions,
+        }
+    };
+};
+
+export default Sessions;
